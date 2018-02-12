5ec55293747addfd47053f5a9b0e0d91f1b14823
manisu <35994839+manisu@users.noreply.github.com>
2018-02-12 15:51:27 +0530

    string
    program

---
+++ string41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+{
+char a[10];
+int i,n;
+printf("\n enter the number");
+scanf("%d",&n);
+printf("\n enter the sting");
+scanf("\n%s",a);
+for(i=0;i<=n;i++)
+{
+printf("\n%s",a);
+}
+getch();
+}
