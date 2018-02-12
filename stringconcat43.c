2e90e10fbe1c44cfce957da74aed4a0a55ccc548
manisu <35994839+manisu@users.noreply.github.com>
2018-02-12 16:14:10 +0530

    Create stringconcat43.c

---
+++ stringconcat43.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+ char a[10],b[10];
+ printf("\n Enter the strings");
+ scanf("%s",a);
+ scanf("%s",b);
+ printf("\n the concatenated string is:%s%s",a,b);
+ getch();
+ }
